9cbdf1012e58d6d7343c4d2d2c6b365309cf4a99
jacobdcl <98243210+jacobdcl@users.noreply.github.com>
2024-12-01 15:40:46 -0500

    layout from stycomp to mui

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import { Box } from '@mui/material';
 import NavTabs from '../NavTabs/NavTabs.jsx';
 import Header from '../Header/Header.jsx';
-import styled from 'styled-components';
-const Child = styled.div`
-    margin: 1rem;
-`;
 
 const Layout = () => {
     return (
-        <div>
+        <Box>
             <Header />
             <NavTabs />
-            <Child>
+            <Box sx={{ margin: '1rem' }}>
                 <Outlet />
-            </Child>
-        </div>
+            </Box>
+        </Box>
     );
 };
 
